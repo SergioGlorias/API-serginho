c626046bee5100efec31bbe65a381869a91afd56
Sérgio Glórias <sergio@serginho.dev>
2022-06-29 00:58:19 +0100

    Add status online e rank color

--- hypixel/png/index.js
+++ hypixel/png/index.js
@@ -7,9 +7,6 @@ const hypixel = new Hypixel.Client(process.env.HYPIXEL_API_KEY, {
     syncWithHeaders: true,
     checkForUpdates: true
 });
-const axios = require("axios")
-const { Buffer } = require('buffer');
-const Readable = require('stream').Readable
 
 module.exports = async (instance, opts, done) => {
 
@@ -30,9 +27,40 @@ module.exports = async (instance, opts, done) => {
         ctx.drawImage(imgSkin, 15, 15, 170, 170)
 
         ctx.font = '50px "League Gothic"'
-        ctx.fillStyle = '#0ec8f7'
-        ctx.fillText((pHy.rank !== "Default" ? ("[" + pHy.rank + "] ") : ("")) + pHy.nickname, 200, 70)
 
+        let rankColor
+        switch (pHy.rank) {
+            case "VIP":
+                rankColor = "#00FF00"
+                break;
+            case "VIP+":
+                rankColor = "#00FF00"
+                break;
+            case "MVP":
+                rankColor = "#00fff6"
+                break;
+            case "MVP+":
+                rankColor = "#00fff6"
+                break;
+            case "MVP++":
+                rankColor = "#ffb200"
+                break;
+            case "Game Master":
+                rankColor = "#00a30d"
+                break;
+            case "ADMIN":
+                rankColor = "#ce0000"
+                break;
+            case "YOUTUBE":
+                rankColor = "#ff0000"
+                break;
+            default:
+                rankColor = "#c1c1c1"
+                break;
+        }
+
+        ctx.fillStyle = rankColor
+        ctx.fillText((pHy.rank !== "Default" ? ("[" + pHy.rank + "] ") : ("")) + pHy.nickname, 200, 70)
         ctx.font = '40px "League Gothic"'
         ctx.fillStyle = '#42aa01'
         ctx.fillText("Level: " + pHy.level.toFixed(0) + " (" + (pHy.levelProgress.percent).toFixed(0) + "%)", 200, 120)
@@ -40,10 +68,22 @@ module.exports = async (instance, opts, done) => {
         ctx.fillText("Achievements: " + pHy.achievementPoints , 200, 170)
         ctx.fillStyle = '#c604f2'
         ctx.fillText("Karma: " + pHy.karma , 500, 120)
+        
+        //add circle solid color to online status
+        ctx.beginPath()
+        ctx.arc(150, 165, 20, 0, 2 * Math.PI)
+        ctx.fillStyle = pHy.isOnline ? '#5dbf0d' : '#c11107'
+        ctx.fill()
+        ctx.closePath()
 
-        reply.header("cache-control", "max-age=0")
+        ctx.beginPath()
+        ctx.arc(150, 165, 15, 0, 2 * Math.PI)
+        ctx.fillStyle = pHy.isOnline ? '#77fc0a' : '#ff3328'
+        ctx.fill()
+        ctx.closePath()
+
+
         reply.type("image/png").send(await canvas.toBuffer())
-
     })
 
     done()
